7936510b384b8a133a2a1f1cb3cb9e68a95d26f7
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-10-11 12:14:22 +0100

    Module 03

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,20 +6,20 @@
 int main() 
 {
 
-	// {
-	// 	std::cout << "\033[31mCreate s1:\033[0m\n";
-	// 	FragTrap s1("BoB");
+	{
+		std::cout << "\033[31mCreate s1:\033[0m\n";
+		FragTrap s1("BoB");
 
-	// 	std::cout << "\n\033[31mCopy construct s2 from s1:\033[0m\n";
-	// 	FragTrap s2 = s1; // Copy constructor
+		std::cout << "\n\033[31mCopy construct s2 from s1:\033[0m\n";
+		FragTrap s2 = s1; // Copy constructor
 
-	// 	std::cout << "\n\033[31mAssign s1 to s3:\033[0m\n";
-	// 	FragTrap s3("Foo");
-	// 	s3 = s1; // Assignment operator
+		std::cout << "\n\033[31mAssign s1 to s3:\033[0m\n";
+		FragTrap s3("Foo");
+		s3 = s1; // Assignment operator
 
-	// 	std::cout << "\n\033[31mdestractors :\033[0m\n";
+		std::cout << "\n\033[31mdestractors :\033[0m\n";
 
-	// }
+	}
 
 
 	{
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,23 +6,23 @@
 int main() 
 {
 
-	// {
-	// 	std::cout << "\033[1;34mCreating d1:\033[0m\n";
-	// 	DiamondTrap d1("Ruby");
+	{
+		std::cout << "\033[1;34mCreating d1:\033[0m\n";
+		DiamondTrap d1("Ruby");
 
-	// 	std::cout << "\n\033[1;34mCalling whoAmI():\033[0m\n";
-	// 	d1.whoAmI();
+		std::cout << "\n\033[1;34mCalling whoAmI():\033[0m\n";
+		d1.whoAmI();
 
-	// 	std::cout << "\n\033[1;34mCopy constructing d2 from d1:\033[0m\n";
-	// 	DiamondTrap d2 = d1;
+		std::cout << "\n\033[1;34mCopy constructing d2 from d1:\033[0m\n";
+		DiamondTrap d2 = d1;
 
-	// 	std::cout << "\n\033[1;34mAssigning d1 to d3:\033[0m\n";
-	// 	DiamondTrap d3("Emerald");
-	// 	d3 = d1;
+		std::cout << "\n\033[1;34mAssigning d1 to d3:\033[0m\n";
+		DiamondTrap d3("Emerald");
+		d3 = d1;
 
-	// 	std::cout << "\n\033[1;34mdestructors :\033[0m\n";
+		std::cout << "\n\033[1;34mdestructors :\033[0m\n";
 
-	// }
+	}
 
 	{
 		std::cout << "\033[1;34mCreating foo:\n\033[0m\n";
@@ -38,14 +38,13 @@ int main()
 
 		foo.attack("Tom"); // ScavTrap => using ScavTrap::attack;
 
-		zoo.takeDamage(50); // ClapTrap
+		zoo.takeDamage(50); // ClapTrap its base classes is called because it’s the only implementation in the hierarchy base ClapTrap no ovveride
 
-		zoo.attack("Sander"); // ScavTrap => using ScavTrap::attack;
+		zoo.attack("Sander");
 
-		zoo_copy.takeDamage(30); // ClapTrap
+		zoo_copy.takeDamage(30); // leave the comment in take Damage in header
 
-		zoo_copy.attack("Vini"); // ClapTrap
-
+		zoo_copy.attack("Vini");
 
 		std::cout << "\n\033[1;34mdestructors :\n\033[0m\n";
 
